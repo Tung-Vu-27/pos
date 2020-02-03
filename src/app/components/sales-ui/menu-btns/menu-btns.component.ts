378f5c851d1cbb44a65884b2a7c549847d3b5531
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-02-02 19:06:14 -0600

    Beginning setup on addButtons in menu

--- src/app/components/sales-ui/menu-btns/menu-btns.component.ts
+++ src/app/components/sales-ui/menu-btns/menu-btns.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from "../../../models/item.model";
+import { ItemService } from "../../../services/item.service";
 
 @Component({
   selector: 'app-menu-btns',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBtnsComponent implements OnInit {
 
-  constructor() { }
+  itemModel: Item;
+
+  constructor(private service: ItemService) {
+    
+   }
 
   ngOnInit() {
   }
 
+  addVanillaCone() {
+    this.itemModel = new Item();
+    this.itemModel.Name = "Vanilla";
+    this.itemModel.Quantity = 1;
+    this.itemModel.UnitPrice = 2.99;
+
+    this.service.postItem(this.itemModel).subscribe(res => {
+      this.service.refreshList();
+    })
+  }
 }
